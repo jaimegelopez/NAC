6c3f52624a23ee6927c2556583ec38d01ca7788b
ComputingDevice <jaimegelopez@gmail.com>
2022-02-21 11:50:05 -0500

    fixed minor issue in compute_multipdf

--- figure_generation/fig3/compute_multipdf.m
+++ figure_generation/fig3/compute_multipdf.m
@@ -4,7 +4,7 @@ temp_cdf = 1 - (1 - cdf).^n;
 pdf = diff(temp_cdf)./diff(init_x);
 x = init_x(1:end-1) ;
 dim_mean = trapz(x+x(2)/2,(x+x(2)/2).*pdf);
-dim_std = sqrt(trapz((x-dim_mean).^2.*pdf));
+dim_std = sqrt(trapz(x+x(2)/2,(x-dim_mean).^2.*pdf));
 area = trapz(x,pdf);
 end
 
